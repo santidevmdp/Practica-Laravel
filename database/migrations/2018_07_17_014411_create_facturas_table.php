fb8e832da9a17afec4644120c902ce7102577e44
santiagomdp33 <santy.mdp.85@gmail.com>
2018-07-16 23:11:32 -0300

    creando migraciones, basico

---
+++ database/migrations/2018_07_17_014411_create_facturas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacturasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facturas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tipo');
+            $table->string('entidad');
+            $table->integer('partida');
+            $table->date('fIngreso');
+            $table->date('fSalida');
+            $table->decimal('deuda');
+            $table->decimal('pagado');
+            $table->decimal('balance');
+            $table->string('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facturas');
+    }
+}
